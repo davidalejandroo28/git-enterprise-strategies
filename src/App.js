65ddd046c2b1b8a43adf8c86eff6d068715465ce
davidalejandroo28 <149003599+davidalejandroo28@users.noreply.github.com>
2025-09-15 22:44:52 -0400

    Include both name lines and image usage in App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! This is our team project.
-        </p>	
+        <p> Hello World! This is our team project </p>
+	<p> My name is David Dorante and this is my project </p>
+        <p> My name is Sami and this is my project </p>	
       </header>
     </div>
   );
